186e22e7c0f0a6063b633c491989d5f6eb41cfe8
nethragithub <nethra.sangeetham@gmail.com>
2025-03-17 19:56:50 +0530

    initial commit

---
+++ 16-install-validate-log-colors-loops.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=$(/tmp/SCRIPT_NAME-$TIMESTAMP.log)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+echo "script running start at:: $TIMESTAMP"
+VALIDATE () {
+    if [$1 -ne 0]
+    then
+       echo -e "$2 is $R failed $N"
+    else
+       echo -e "$2 is $G success $N"
+    fi
+if [$USERID -ne 0]
+then 
+    echo "please run script with root access"
+else
+    echo "you are root user"
+fi
+for i in $@
+do
+    echo "package to install:: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [$? -ne 0]
+    then 
+       echo "$i is installed ...skipping"
+    else
+       dnf install $i -y &>>LOGFILE
+       VALIDATE $1 "installation of $i"
+    fi    
+done
