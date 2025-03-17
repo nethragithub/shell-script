3b21a4caa8c8464e7e9672b95171710e46dcb4d5
nethragithub <nethra.sangeetham@gmail.com>
2025-03-17 18:58:58 +0530

    initial commit

--- 13-installation-logs-functions.sh
+++ 13-installation-logs-functions.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=$(/tmp/$SCRIPT_NAME-$TIMESTAMP.log)
 USERID=$(id -u)
 VALIDATE () {
 if [$1 -ne 0]
 then
-    echo "$2 is failed"
+    echo "installation of $2 is failed"
     exit 1
 else
-    echo "$2 is suceess"
+    echo "installation of $2 is suceess"
 fi            
 }
 if [$USERID -ne 0]
@@ -16,7 +19,7 @@ then
 else
     echo "you are root user"
 fi
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing mysql"
-dnf install nginx -y
+dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "installing nginx"          
---
+++ 14-installation-validate-logs-colours.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=$(/tmp/$SCRIPT_NAME-$TIMESTAMP.log)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+echo "script starting at:: ${TIMESTAMP}"
+VALIDATE () {
+if [$1 -ne 0]
+then
+    echo -e "installation of $2 is $R failed $N"
+    exit 1
+else
+    echo -e "installation of $2 is $G suceess $N"
+fi            
+}
+if [$USERID -ne 0]
+then
+    echo "run script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "installing mysql"
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "installing nginx"          
