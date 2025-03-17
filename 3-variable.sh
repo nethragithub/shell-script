5ecd58906211718f3a5768a56c036207d8622437
nethragithub <nethra.sangeetham@gmail.com>
2025-03-17 09:56:56 +0530

    initial commit

--- 3-variable.sh
+++ 3-variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PERSON1:Ramesh
 PERSON2::suresh
-echo "$1: hello $2, how are you?"
-echo "$2: hi $1, fine, what about you?"
+echo "$PERSON1: hello $PERSON2, how are you?"
+echo "$PERSON2: hi $PERSON1, fine, what about you?"
