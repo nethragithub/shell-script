f6f390f58dbf1964714e54b89c3f4b057467318e
nethragithub <nethra.sangeetham@gmail.com>
2025-03-18 12:29:30 +0530

    initial commit

--- 13-installation-logs-functions.sh
+++ 13-installation-logs-functions.sh
@@ -3,14 +3,15 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=$(/tmp/$SCRIPT_NAME-$TIMESTAMP.log)
 USERID=$(id -u)
+
 VALIDATE () {
-if [$1 -ne 0]
-then
-    echo "installation of $2 is failed"
-    exit 1
-else
-    echo "installation of $2 is suceess"
-fi            
+   if [ $1 -ne 0 ]
+   then
+       echo "installation of $2 is failed"
+       exit 1
+    else
+       echo "installation of $2 is suceess"
+    fi            
 }
 if [$USERID -ne 0]
 then
@@ -19,7 +20,9 @@ then
 else
     echo "you are root user"
 fi
+
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing mysql"
+
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "installing nginx"          
