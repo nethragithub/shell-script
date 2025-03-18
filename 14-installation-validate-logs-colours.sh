cdddfbabef6b5449640872bc8909125b3c68acfc
nethragithub <nethra.sangeetham@gmail.com>
2025-03-18 12:10:46 +0530

    initial commit

--- 14-installation-validate-logs-colours.sh
+++ 14-installation-validate-logs-colours.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=$(/tmp/$SCRIPT_NAME-$TIMESTAMP.log)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 echo "script starting at:: ${TIMESTAMP}"
+
 VALIDATE () {
-if [$1 -ne 0]
-then
-    echo -e "installation of $2 is $R failed $N"
-    exit 1
-else
-    echo -e "installation of $2 is $G suceess $N"
-fi            
+    if [$1 -ne 0]
+    then
+        echo -e "installation of $2 is $R failed $N"
+        exit 1
+    else
+        echo -e "installation of $2 is $G suceess $N"
+    fi            
 }
 if [$USERID -ne 0]
 then
