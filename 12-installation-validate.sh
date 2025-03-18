9aedacc6f43783132dc10f20770d3af4b431a376
nethragithub <nethra.sangeetham@gmail.com>
2025-03-18 12:24:30 +0530

    initial commit

--- 12-installation-validate.sh
+++ 12-installation-validate.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 USERID=$(id -u)
+
 VALIDATE () {
-if [$1 -ne 0]
-then
-    echo "$2 is failed"
-    exit 1
-else
-    echo "$2 is suceess"
-fi            
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed"
+        exit 1
+    else
+        echo "$2 is suceess"
+    fi            
 }
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "run script with root access"
     exit 1
 else
     echo "you are root user"
 fi
+
 dnf install mysql -y
 VALIDATE $? "installing mysql"
+
 dnf install nginx -y
 VALIDATE $? "installing nginx"          
