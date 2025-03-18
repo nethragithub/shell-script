cf85c7b4dc7efb4c4d8b0fa2989b335d9e5d8d14
nethragithub <nethra.sangeetham@gmail.com>
2025-03-18 12:32:29 +0530

    initial commit

--- 13-installation-logs-functions.sh
+++ 13-installation-logs-functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=$(/tmp/$SCRIPT_NAME-$TIMESTAMP.log)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 USERID=$(id -u)
 
 VALIDATE () {
@@ -13,7 +13,7 @@ VALIDATE () {
        echo "installation of $2 is suceess"
     fi            
 }
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "run script with root access"
     exit 1
