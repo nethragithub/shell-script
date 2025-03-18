214c523e8c8d86892b58d4de35fe01384d151692
nethragithub <nethra.sangeetham@gmail.com>
2025-03-18 12:00:47 +0530

    initial commit

--- 16-install-validate-log-colors-loops.sh
+++ 16-install-validate-log-colors-loops.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=$(/tmp/SCRIPT_NAME-$TIMESTAMP.log)
+LOGFILE=/tmp/SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
