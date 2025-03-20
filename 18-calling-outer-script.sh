f5bc82a69217c1b9b6e4d5d8b46fc426c70f44ad
nethragithub <nethra.sangeetham@gmail.com>
2025-03-20 11:45:33 +0530

    initial commit

--- 18-calling-outer-script.sh
+++ 18-calling-outer-script.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 source ./17-commom.sh
-check_root
+check-root
 for i in $@
 do
-    echo "package to install:: $i"
-    dnf list installed $i &>>$LOGFILE
-    if [ $? -ne 0 ]
-    then 
-       echo "$i is installed ...skipping"
-    else
-       dnf install $i -y &>>LOGFILE
-    #   VALIDATE $1 "installation of $i"
-    fi    
-done    
+dnf install -y $1 &>>$LOGFILE
