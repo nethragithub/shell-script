3a0bb875df677cb9e2a2356d7188ba2ec85da3f9
nethragithub <nethra.sangeetham@gmail.com>
2025-03-25 15:25:40 +0530

    initial commit

--- 23-high-cpumemory.sh
+++ 23-high-cpumemory.sh
@@ -4,7 +4,7 @@ echo "top 5 cpu consuming process:"
 ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6
 echo " "
 echo "checking for the process that exceeds $THRESHOLD% cpu usage"
-ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | awk "NR>1" "if $4 > $THRESHOLD" print $0
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | awk 'NR>1 {if ($4 > '"$THRESHOLD"') print $0}''
 while read - r pid,ppid,cmd,cpu
 do
   echo "Alert:: process $pid $cmd is using $cpu% cpu!"
