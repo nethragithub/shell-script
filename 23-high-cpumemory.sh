aef58e6c2446edf3e90139dc5f8b2e1105d8af50
nethragithub <nethra.sangeetham@gmail.com>
2025-03-25 15:30:16 +0530

    initial commit

--- 23-high-cpumemory.sh
+++ 23-high-cpumemory.sh
@@ -8,5 +8,5 @@ ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | awk 'NR>1 {if ($4 > '"$THRESHOLD"') prin
 while read - r pid,ppid,cmd,cpu
 do
   echo "Alert:: process $pid $cmd is using $cpu% cpu!"
-  snail -s "cpu high usage alert" nethra.sangeetham@gmail.com << "process $pip $cmd is using $cpu% cpu!"
+  snail -s "cpu high usage alert" nethra.sangeetham@gmail.com <<< "process $pip $cmd is using $cpu% cpu!"
 done  
